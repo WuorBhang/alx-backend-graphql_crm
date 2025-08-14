ad42d90a7cdec727533564976095ec10625edcca
Wuor Bhang <143351848+WuorBhang@users.noreply.github.com>
2025-08-14 21:56:24 +0300

    Update schema.py

--- graphql_crm/schema.py
+++ graphql_crm/schema.py
@@ -1,12 +1,22 @@
 # graphql_crm/schema.py
 
 import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String()
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
+    def resolve_hello(self, info):
+        return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
+
+# import graphene
+# from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+# class Query(CRMQuery, graphene.ObjectType):
+#     pass
+
+# class Mutation(CRMMutation, graphene.ObjectType):
+#     pass
+
+# schema = graphene.Schema(query=Query, mutation=Mutation)
