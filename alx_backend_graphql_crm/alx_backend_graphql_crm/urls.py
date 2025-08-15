f27b42b19b7989a8e7d7c2f1ab1538942d3adab3
WuorBhang <uhuribhang211@gmail.com>
2025-08-15 20:40:04 +0300

    updates changes again

--- alx_backend_graphql_crm/alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/alx_backend_graphql_crm/urls.py
@@ -19,15 +19,7 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
-# Import the schema instance directly so GraphQLView doesn't need to resolve it each time
-from alx_backend_graphql_crm.schema import schema
-
 urlpatterns = [
-<<<<<<<< HEAD:alx_backend_graphql_crm/alx_backend_graphql_crm/urls.py
     path('admin/', admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-========
-    path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
->>>>>>>> 3d5371b352cc2b853c543c3756ae8dd29199bce3:alx_backend_graphql_crm/urls.py
 ]
