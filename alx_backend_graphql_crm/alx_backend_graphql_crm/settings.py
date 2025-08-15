34d5c4a6c34f311426913e12bb1291c179fb49e5
WuorBhang <uhuribhang211@gmail.com>
2025-08-15 20:19:17 +0300

    changes to the updates branch

--- alx_backend_graphql_crm/alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/alx_backend_graphql_crm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%b2vl%3+3ho1-l6$bvg7+5pyq2cgks_##)$!%$vsf11a*ss#v5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
